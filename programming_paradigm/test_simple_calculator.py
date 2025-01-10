c1fbdcdb395f3ea7308ed3af1fa3aa2ac71c4bf9
danicex <dantegaefe@gmail.com>
2025-01-10 19:57:48 +0100

    changed

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -11,8 +11,12 @@ class TestSimpleCalculator(unittest.TestCase):
         """Test the addition method."""
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, 1), 0)
+
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(-1, 1), 0)
+    def test_divide(self):
         self.assertEqual(self.calc.divide(-1, 1), 0)
+    def test_multiply(self):
         self.assertEqual(self.calc.multiply(-1, 1), 0)
         # Add more assertions to thoroughly test the add method.
 
